4051b18aa54cde9fb15062dd9b07d6f42f1040a2
IglooSyrup <darckuvina@gmail.com>
2021-12-16 19:18:10 -0500

    initial blank project commit

---
+++ Githubactiontests/src/main/java/com/igloosyrup/githubactionstest/GithubactionstestApplication.java
@@ -0,0 +1,13 @@
+package com.igloosyrup.githubactionstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionstestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubactionstestApplication.class, args);
+    }
+
+}
---
+++ Githubactiontests/src/test/java/com/igloosyrup/githubactionstest/GithubactionstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igloosyrup.githubactionstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
